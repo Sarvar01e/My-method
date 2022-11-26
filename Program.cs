5ff411f1a63075fc7e94fba7c0930f67362580f2
Sarvar01e <sernazarov185@gmail.com>
2022-11-27 02:01:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace My_method
+{
+    internal class Program
+    {
+
+        static  string Drowsymbol(string symbolfordrow, int howmanysymbol) 
+        {
+            string drow;
+            drow = default;
+            for (int i = 0; i < howmanysymbol; i++)
+            {
+                drow = drow + symbolfordrow;
+            }
+            return drow;
+        }
+        static void Main(string[] args)
+        {
+            string symbol;
+            int size;
+            string prit;
+
+            Console.Write("Enter symbol:");
+            symbol = Console.ReadLine();
+            Console.Write("Enter size:");
+            size = int.Parse(Console.ReadLine());
+
+            prit = Drowsymbol(symbol, size);
+            Console.WriteLine(prit);
+        }
+    }
+}
